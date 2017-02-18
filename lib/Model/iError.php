c44ecc731139fa87c0638229a3cdd2aa0242f0cc
Maxence Lange <maxence@pontapreta.net>
2017-02-18 00:33:03 -0100

    iError()

---
+++ lib/Model/iError.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Circles - bring cloud-users closer
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Maxence Lange <maxence@pontapreta.net>
+ * @copyright 2017
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Circles\Model;
+
+class iError {
+
+	private $messages = array();
+	private $code;
+
+	function __construct() {
+	}
+
+	public function setMessage($message) {
+		array_push($this->messages, $message);
+
+		return $this;
+	}
+
+	public function getMessages() {
+		return $this->messages;
+	}
+
+
+	public function setCode($code) {
+		$this->code = $code;
+
+		return $this;
+	}
+
+	public function getCode() {
+		return $this->code;
+	}
+
+
+	public function toArray() {
+		return array(
+			'code'     => $this->getCode(),
+			'messages' => $this->getMessages()
+		);
+	}
+
+
+}
