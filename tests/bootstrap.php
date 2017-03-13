025e7a1676af55a8067abb1326627398c930882a
Maxence Lange <maxence@nextcloud.com>
2017-03-13 12:31:42 -0100

    test env

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,18 +10,11 @@ require_once __DIR__ . '/../../../lib/base.php';
 // Fix for "Autoload path not allowed: .../tests/lib/testcase.php"
 \OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
 
-// Fix for "Autoload path not allowed: .../activity/tests/testcase.php"
 \OC_App::loadApp('circles');
-
-// Fix for "Autoload path not allowed: .../files/lib/activity.php"
-//\OC_App::loadApp('files');
-
-// Fix for "Autoload path not allowed: .../files_sharing/lib/activity.php"
-//\OC_App::loadApp('files_sharing');
-
 
 if (!class_exists('PHPUnit_Framework_TestCase')) {
 	require_once('PHPUnit/Autoload.php');
 }
 
 OC_Hook::clear();
+
