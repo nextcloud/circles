54240b616336b2027280ebee9a39fc5e2add022d
Maxence Lange <maxence@nextcloud.com>
2017-06-08 00:20:29 -0100

    fixing git
    Signed-off-by: Maxence Lange <maxence@nextcloud.com>

---
+++ lib/Activity/SettingShares.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace OCA\Circles\Activity;
+
+
+use OCP\Activity\ISetting;
+use OCP\IL10N;
+
+class SettingShares implements ISetting {
+
+	/** @var IL10N */
+	protected $l10n;
+
+	/**
+	 * @param IL10N $l10n
+	 */
+	public function __construct(IL10N $l10n) {
+		$this->l10n = $l10n;
+	}
+
+	/**
+	 * @return string Lowercase a-z and underscore only identifier
+	 * @since 11.0.0
+	 */
+	public function getIdentifier() {
+		return 'circles_shares';
+	}
+
+	/**
+	 * @return string A translated string
+	 * @since 11.0.0
+	 */
+	public function getName() {
+		return $this->l10n->t('A file is shared with a <strong>Circle</strong> you are a member');
+	}
+
+	/**
+	 * @return int whether the filter should be rather on the top or bottom of
+	 * the admin section. The filters are arranged in ascending order of the
+	 * priority values. It is required to return a value between 0 and 100.
+	 * @since 11.0.0
+	 */
+	public function getPriority() {
+		return 60;
+	}
+
+	/**
+	 * @return bool True when the option can be changed for the stream
+	 * @since 11.0.0
+	 */
+	public function canChangeStream() {
+		return true;
+	}
+
+	/**
+	 * @return bool True when the option can be changed for the stream
+	 * @since 11.0.0
+	 */
+	public function isDefaultEnabledStream() {
+		return true;
+	}
+
+	/**
+	 * @return bool True when the option can be changed for the mail
+	 * @since 11.0.0
+	 */
+	public function canChangeMail() {
+		return true;
+	}
+
+	/**
+	 * @return bool True when the option can be changed for the stream
+	 * @since 11.0.0
+	 */
+	public function isDefaultEnabledMail() {
+		return true;
+	}
+}
+
