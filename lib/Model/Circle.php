5f0f9c7d7cf24580191397a9aa0e994fad139b9e
Maxence Lange <maxence@nextcloud.com>
2017-06-15 09:20:03 -0100

    renaming
    Signed-off-by: Maxence Lange <maxence@nextcloud.com>

--- lib/Model/Circle.php
+++ lib/Model/Circle.php
@@ -149,10 +149,10 @@ class Circle extends BaseCircle implements \JsonSerializable {
 		$circle->setCreation($arr['creation']);
 
 		if (key_exists('user', $arr)) {
-			$circle->setUser(Member::fromArray2($l10n, $arr['user']));
+			$circle->setUser(Member::fromArray($l10n, $arr['user']));
 		}
 		if (key_exists('owner', $arr)) {
-			$circle->setOwner(Member::fromArray2($l10n, $arr['owner']));
+			$circle->setOwner(Member::fromArray($l10n, $arr['owner']));
 		}
 
 		return $circle;
