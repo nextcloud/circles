4a54fd1f5f4fa062b5958c8003d5702d662048fe
Maxence Lange <maxence@nextcloud.com>
2017-08-12 20:33:33 +0200

    spliting
    Signed-off-by: Maxence Lange <maxence@nextcloud.com>

---
+++ lib/Activity/BaseProvider.php
@@ -0,0 +1,316 @@
+<?php
+
+/**
+ * Circles - Bring cloud-users closer together.
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Maxence Lange <maxence@pontapreta.net>
+ * @copyright 2017
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Circles\Activity;
+
+use OCA\Circles\Api\v1\Circles;
+use OCA\Circles\Model\Circle;
+use OCA\Circles\Model\FederatedLink;
+use OCA\Circles\Model\Member;
+use OCA\Circles\Service\MiscService;
+use OCP\Activity\IEvent;
+use OCP\Activity\IManager;
+use OCP\IL10N;
+use OCP\IURLGenerator;
+
+class BaseProvider {
+
+
+	/** @var MiscService */
+	protected $miscService;
+
+	/** @var IL10N */
+	protected $l10n;
+
+	/** @var IURLGenerator */
+	protected $url;
+
+	/** @var IManager */
+	protected $activityManager;
+
+	public function __construct(
+		IURLGenerator $url, IManager $activityManager, IL10N $l10n, MiscService $miscService
+	) {
+		$this->url = $url;
+		$this->activityManager = $activityManager;
+		$this->l10n = $l10n;
+		$this->miscService = $miscService;
+	}
+
+
+	/**
+	 * general function to generate Circle event.
+	 *
+	 * @param IEvent $event
+	 * @param Circle $circle
+	 * @param FederatedLink $remote
+	 * @param string $ownEvent
+	 * @param string $othersEvent
+	 *
+	 * @return IEvent
+	 */
+	protected function parseCircleEvent(
+		IEvent &$event, Circle $circle, $remote, $ownEvent, $othersEvent
+	) {
+		$data = [
+			'author' => $this->generateViewerParameter($circle),
+			'circle' => $this->generateCircleParameter($circle),
+			'remote' => ($remote === null) ? '' : $this->generateRemoteCircleParameter($remote)
+		];
+
+		if ($this->isViewerTheAuthor($circle, $this->activityManager->getCurrentUserId())) {
+			return $event->setRichSubject($ownEvent, $data);
+		}
+
+		return $event->setRichSubject($othersEvent, $data);
+	}
+
+
+	/**
+	 * general function to generate Member event.
+	 *
+	 * @param Circle $circle
+	 * @param $member
+	 * @param IEvent $event
+	 * @param $ownEvent
+	 * @param $othersEvent
+	 *
+	 * @return IEvent
+	 */
+	protected function parseMemberEvent(
+		IEvent &$event, Circle $circle, Member $member, $ownEvent, $othersEvent
+	) {
+		$data = [
+			'circle' => $this->generateCircleParameter($circle),
+			'member' => $this->generateUserParameter($member)
+		];
+
+		if ($member->getUserId() === $this->activityManager->getCurrentUserId()
+		) {
+			return $event->setRichSubject($ownEvent, $data);
+		}
+
+		return $event->setRichSubject($othersEvent, $data);
+	}
+
+
+	/**
+	 * general function to generate Link event.
+	 *
+	 * @param Circle $circle
+	 * @param FederatedLink $remote
+	 * @param IEvent $event
+	 * @param string $line
+	 *
+	 * @return IEvent
+	 */
+	protected function parseLinkEvent(IEvent &$event, Circle $circle, FederatedLink $remote, $line
+	) {
+		$data = [
+			'circle' => $this->generateCircleParameter($circle),
+			'remote' => $this->generateRemoteCircleParameter($remote)
+		];
+
+		return $event->setRichSubject($line, $data);
+	}
+
+
+	/**
+	 * general function to generate Circle+Member event.
+	 *
+	 * @param Circle $circle
+	 * @param Member $member
+	 * @param IEvent $event
+	 * @param string $ownEvent
+	 * @param string $othersEvent
+	 *
+	 * @return IEvent
+	 */
+	protected function parseCircleMemberEvent(
+		IEvent &$event, Circle $circle, Member $member, $ownEvent, $othersEvent
+	) {
+		$data = [
+			'author' => $this->generateViewerParameter($circle),
+			'circle' => $this->generateCircleParameter($circle),
+			'member' => $this->generateUserParameter($member),
+			'group'  => $this->generateGroupParameter($member),
+		];
+
+		if ($this->isViewerTheAuthor($circle, $this->activityManager->getCurrentUserId())) {
+			return $event->setRichSubject($ownEvent, $data);
+		}
+
+		return $event->setRichSubject($othersEvent, $data);
+	}
+
+
+	/**
+	 * general function to generate Circle+Member advanced event.
+	 *
+	 * @param Circle $circle
+	 * @param Member $member
+	 * @param IEvent $event
+	 *\
+	 * @param $ownEvent
+	 * @param $targetEvent
+	 * @param $othersEvent
+	 *
+	 * @return IEvent
+	 */
+	protected function parseCircleMemberAdvancedEvent(
+		IEvent &$event, Circle $circle, Member $member, $ownEvent, $targetEvent, $othersEvent
+	) {
+		$data = [
+			'author' => $this->generateViewerParameter($circle),
+			'circle' => $this->generateCircleParameter($circle),
+			'member' => $this->generateUserParameter($member)
+		];
+
+		if ($this->isViewerTheAuthor($circle, $this->activityManager->getCurrentUserId())) {
+			return $event->setRichSubject($ownEvent, $data);
+		}
+
+		if ($member->getUserId() === $this->activityManager->getCurrentUserId()) {
+			return $event->setRichSubject($targetEvent, $data);
+		}
+
+		return $event->setRichSubject($othersEvent, $data);
+	}
+
+
+	/**
+	 * @param IEvent $event
+	 */
+	protected function generateParsedSubject(IEvent &$event) {
+		$subject = $event->getRichSubject();
+		$params = $event->getRichSubjectParameters();
+		$ak = array_keys($params);
+		foreach ($ak as $k) {
+			if (is_array($params[$k])) {
+				$subject = str_replace('{' . $k . '}', $params[$k]['parsed'], $subject);
+			}
+		}
+
+		$event->setParsedSubject($subject);
+	}
+
+
+	/**
+	 * @param Circle $circle
+	 * @param string $userId
+	 *
+	 * @return bool
+	 */
+	protected function isViewerTheAuthor(Circle $circle, $userId) {
+		if ($circle->getViewer() === null) {
+			return false;
+		}
+
+		if ($circle->getViewer()
+				   ->getUserId() === $userId) {
+			return true;
+		}
+
+		return false;
+	}
+
+
+	/**
+	 * @param Circle $circle
+	 *
+	 * @return string|array <string,string|integer>
+	 */
+	protected function generateViewerParameter(Circle $circle) {
+		if ($circle->getViewer() === null) {
+			return '';
+		}
+
+		return $this->generateUserParameter($circle->getViewer());
+	}
+
+
+	/**
+	 * @param Circle $circle
+	 *
+	 * @return array<string,string|integer>
+	 */
+	protected function generateCircleParameter(Circle $circle) {
+		return [
+			'type'   => 'circle',
+			'id'     => $circle->getId(),
+			'name'   => $circle->getName(),
+			'parsed' => $circle->getName(),
+			'link'   => Circles::generateLink($circle->getUniqueId())
+		];
+	}
+
+
+	/**
+	 * @param FederatedLink $link
+	 *
+	 * @return array<string,string|integer>
+	 */
+	protected function generateRemoteCircleParameter(FederatedLink $link) {
+		return [
+			'type'   => 'circle',
+			'id'     => $link->getUniqueId(),
+			'name'   => $link->getToken() . '@' . $link->getAddress(),
+			'parsed' => $link->getToken() . '@' . $link->getAddress()
+		];
+	}
+
+
+	/**
+	 * @param Member $member
+	 *
+	 * @return array <string,string|integer>
+	 */
+	protected function generateUserParameter(Member $member) {
+		return [
+			'type'   => 'user',
+			'id'     => $member->getUserId(),
+			'name'   => $this->miscService->getDisplayName($member->getUserId(), true),
+			'parsed' => $this->miscService->getDisplayName($member->getUserId(), true)
+		];
+	}
+
+
+	/**
+	 * @param Member $group
+	 *
+	 * @return array <string,string|integer>
+	 */
+	protected function generateGroupParameter($group) {
+		return [
+			'type'   => 'group',
+			'id'     => $group->getUserId(),
+			'name'   => $group->getUserId(),
+			'parsed' => $group->getUserId()
+		];
+	}
+
+}
--- lib/Activity/Provider.php
+++ lib/Activity/Provider.php
@@ -1,47 +1,43 @@
 <?php
 
+/**
+ * Circles - Bring cloud-users closer together.
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Maxence Lange <maxence@pontapreta.net>
+ * @copyright 2017
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 namespace OCA\Circles\Activity;
 
-
 use Exception;
 use InvalidArgumentException;
-use OCA\Circles\Api\v1\Circles;
 use OCA\Circles\Model\Circle;
 use OCA\Circles\Model\FederatedLink;
 use OCA\Circles\Model\Member;
 use OCA\Circles\Service\CirclesService;
-use OCA\Circles\Service\MiscService;
 use OCP\Activity\IEvent;
-use OCP\Activity\IManager;
 use OCP\Activity\IProvider;
-use OCP\IL10N;
-use OCP\IURLGenerator;
 use OpenCloud\Common\Exceptions\InvalidArgumentError;
 
-
-class Provider implements IProvider {
-
-	/** @var MiscService */
-	protected $miscService;
-
-	/** @var IL10N */
-	protected $l10n;
-
-	/** @var IURLGenerator */
-	protected $url;
-
-	/** @var IManager */
-	protected $activityManager;
-
-	public function __construct(
-		IURLGenerator $url, IManager $activityManager, IL10N $l10n, MiscService $miscService
-	) {
-		$this->url = $url;
-		$this->activityManager = $activityManager;
-		$this->l10n = $l10n;
-		$this->miscService = $miscService;
-	}
+class Provider extends BaseProvider implements IProvider {
 
 
 	/**
@@ -117,6 +113,39 @@ class Provider implements IProvider {
 		}
 
 		return $event;
+	}
+
+
+	/**
+	 * @param Circle $circle
+	 * @param IEvent $event
+	 * @param array $params
+	 *
+	 * @return IEvent
+	 * @throws Exception
+	 */
+	private function parseAsModerator(IEvent &$event, Circle $circle, $params) {
+		if ($event->getType() !== 'circles_as_moderator') {
+			return $event;
+		}
+
+		try {
+			if (key_exists('member', $params)) {
+				return $this->parseMemberAsModerator($event, $circle);
+			}
+
+			if (key_exists('group', $params)) {
+				return $this->parseGroupAsModerator($event, $circle);
+			}
+
+			if (key_exists('link', $params)) {
+				return $this->parseLinkAsModerator($event, $circle);
+			}
+
+			throw new InvalidArgumentError();
+		} catch (Exception $e) {
+			throw $e;
+		}
 	}
 
 
@@ -228,6 +257,7 @@ class Provider implements IProvider {
 		}
 	}
 
+
 	/**
 	 * Parse on Subject 'member_left' on a closed circle when user were not yet a member.
 	 * If status is Invited we say that member rejected his invitation.
@@ -299,7 +329,7 @@ class Provider implements IProvider {
 	private function parseSubjectNonMemberRemoveClosedCircle(
 		IEvent &$event, Circle $circle, Member $member
 	) {
-	if ($member->getStatus() === Member::STATUS_REQUEST) {
+		if ($member->getStatus() === Member::STATUS_REQUEST) {
 			return $this->parseCircleMemberAdvancedEvent(
 				$event, $circle, $member,
 				$this->l10n->t("You dismissed {member}'s request to join {circle}"),
@@ -314,39 +344,6 @@ class Provider implements IProvider {
 			$this->l10n->t('Your invitation to join {circle} has been cancelled by {author}'),
 			$this->l10n->t("{author} cancelled {member}'s invitation to join {circle}")
 		);
-	}
-
-
-	/**
-	 * @param Circle $circle
-	 * @param IEvent $event
-	 * @param array $params
-	 *
-	 * @return IEvent
-	 * @throws Exception
-	 */
-	private function parseAsModerator(IEvent &$event, Circle $circle, $params) {
-		if ($event->getType() !== 'circles_as_moderator') {
-			return $event;
-		}
-
-		try {
-			if (key_exists('member', $params)) {
-				return $this->parseMemberAsModerator($event, $circle);
-			}
-
-			if (key_exists('group', $params)) {
-				return $this->parseGroupAsModerator($event, $circle);
-			}
-
-			if (key_exists('link', $params)) {
-				return $this->parseLinkAsModerator($event, $circle);
-			}
-
-			throw new InvalidArgumentError();
-		} catch (Exception $e) {
-			throw $e;
-		}
 	}
 
 
@@ -538,254 +535,4 @@ class Provider implements IProvider {
 		throw new InvalidArgumentException();
 	}
 
-
-	/**
-	 * general function to generate Circle event.
-	 *
-	 * @param IEvent $event
-	 * @param Circle $circle
-	 * @param FederatedLink $remote
-	 * @param string $ownEvent
-	 * @param string $othersEvent
-	 *
-	 * @return IEvent
-	 */
-	private function parseCircleEvent(
-		IEvent &$event, Circle $circle, $remote, $ownEvent, $othersEvent
-	) {
-		$data = [
-			'author' => $this->generateViewerParameter($circle),
-			'circle' => $this->generateCircleParameter($circle),
-			'remote' => ($remote === null) ? '' : $this->generateRemoteCircleParameter($remote)
-		];
-
-		if ($this->isViewerTheAuthor($circle, $this->activityManager->getCurrentUserId())) {
-			return $event->setRichSubject($ownEvent, $data);
-		}
-
-		return $event->setRichSubject($othersEvent, $data);
-	}
-
-
-	/**
-	 * general function to generate Member event.
-	 *
-	 * @param Circle $circle
-	 * @param $member
-	 * @param IEvent $event
-	 * @param $ownEvent
-	 * @param $othersEvent
-	 *
-	 * @return IEvent
-	 */
-	private function parseMemberEvent(
-		IEvent &$event, Circle $circle, Member $member, $ownEvent, $othersEvent
-	) {
-		$data = [
-			'circle' => $this->generateCircleParameter($circle),
-			'member' => $this->generateUserParameter($member)
-		];
-
-		if ($member->getUserId() === $this->activityManager->getCurrentUserId()
-		) {
-			return $event->setRichSubject($ownEvent, $data);
-		}
-
-		return $event->setRichSubject($othersEvent, $data);
-	}
-
-
-	/**
-	 * general function to generate Link event.
-	 *
-	 * @param Circle $circle
-	 * @param FederatedLink $remote
-	 * @param IEvent $event
-	 * @param string $line
-	 *
-	 * @return IEvent
-	 */
-	private function parseLinkEvent(IEvent &$event, Circle $circle, FederatedLink $remote, $line) {
-		$data = [
-			'circle' => $this->generateCircleParameter($circle),
-			'remote' => $this->generateRemoteCircleParameter($remote)
-		];
-
-		return $event->setRichSubject($line, $data);
-	}
-
-
-	/**
-	 * general function to generate Circle+Member event.
-	 *
-	 * @param Circle $circle
-	 * @param Member $member
-	 * @param IEvent $event
-	 * @param string $ownEvent
-	 * @param string $othersEvent
-	 *
-	 * @return IEvent
-	 */
-	private function parseCircleMemberEvent(
-		IEvent &$event, Circle $circle, Member $member, $ownEvent, $othersEvent
-	) {
-		$data = [
-			'author' => $this->generateViewerParameter($circle),
-			'circle' => $this->generateCircleParameter($circle),
-			'member' => $this->generateUserParameter($member),
-			'group'  => $this->generateGroupParameter($member),
-		];
-
-		if ($this->isViewerTheAuthor($circle, $this->activityManager->getCurrentUserId())) {
-			return $event->setRichSubject($ownEvent, $data);
-		}
-
-		return $event->setRichSubject($othersEvent, $data);
-	}
-
-
-	/**
-	 * general function to generate Circle+Member advanced event.
-	 *
-	 * @param Circle $circle
-	 * @param Member $member
-	 * @param IEvent $event
-	 *\
-	 * @param $ownEvent
-	 * @param $targetEvent
-	 * @param $othersEvent
-	 *
-	 * @return IEvent
-	 */
-	private function parseCircleMemberAdvancedEvent(
-		IEvent &$event, Circle $circle, Member $member, $ownEvent, $targetEvent, $othersEvent
-	) {
-		$data = [
-			'author' => $this->generateViewerParameter($circle),
-			'circle' => $this->generateCircleParameter($circle),
-			'member' => $this->generateUserParameter($member)
-		];
-
-		if ($this->isViewerTheAuthor($circle, $this->activityManager->getCurrentUserId())) {
-			return $event->setRichSubject($ownEvent, $data);
-		}
-
-		if ($member->getUserId() === $this->activityManager->getCurrentUserId()) {
-			return $event->setRichSubject($targetEvent, $data);
-		}
-
-		return $event->setRichSubject($othersEvent, $data);
-	}
-
-
-	/**
-	 * @param IEvent $event
-	 */
-	private function generateParsedSubject(IEvent &$event) {
-		$subject = $event->getRichSubject();
-		$params = $event->getRichSubjectParameters();
-		$ak = array_keys($params);
-		foreach ($ak as $k) {
-			if (is_array($params[$k])) {
-				$subject = str_replace('{' . $k . '}', $params[$k]['parsed'], $subject);
-			}
-		}
-
-		$event->setParsedSubject($subject);
-	}
-
-
-	/**
-	 * @param Circle $circle
-	 * @param string $userId
-	 *
-	 * @return bool
-	 */
-	private function isViewerTheAuthor(Circle $circle, $userId) {
-		if ($circle->getViewer() === null) {
-			return false;
-		}
-
-		if ($circle->getViewer()
-				   ->getUserId() === $userId) {
-			return true;
-		}
-
-		return false;
-	}
-
-
-	/**
-	 * @param Circle $circle
-	 *
-	 * @return string|array <string,string|integer>
-	 */
-	private function generateViewerParameter(Circle $circle) {
-		if ($circle->getViewer() === null) {
-			return '';
-		}
-
-		return $this->generateUserParameter($circle->getViewer());
-	}
-
-
-	/**
-	 * @param Circle $circle
-	 *
-	 * @return array<string,string|integer>
-	 */
-	private function generateCircleParameter(Circle $circle) {
-		return [
-			'type'   => 'circle',
-			'id'     => $circle->getId(),
-			'name'   => $circle->getName(),
-			'parsed' => $circle->getName(),
-			'link'   => Circles::generateLink($circle->getUniqueId())
-		];
-	}
-
-
-	/**
-	 * @param FederatedLink $link
-	 *
-	 * @return array<string,string|integer>
-	 */
-	private function generateRemoteCircleParameter(FederatedLink $link) {
-		return [
-			'type'   => 'circle',
-			'id'     => $link->getUniqueId(),
-			'name'   => $link->getToken() . '@' . $link->getAddress(),
-			'parsed' => $link->getToken() . '@' . $link->getAddress()
-		];
-	}
-
-
-	/**
-	 * @param Member $member
-	 *
-	 * @return array <string,string|integer>
-	 */
-	private function generateUserParameter(Member $member) {
-		return [
-			'type'   => 'user',
-			'id'     => $member->getUserId(),
-			'name'   => $this->miscService->getDisplayName($member->getUserId(), true),
-			'parsed' => $this->miscService->getDisplayName($member->getUserId(), true)
-		];
-	}
-
-
-	/**
-	 * @param Member $group
-	 *
-	 * @return array <string,string|integer>
-	 */
-	private function generateGroupParameter($group) {
-		return [
-			'type'   => 'group',
-			'id'     => $group->getUserId(),
-			'name'   => $group->getUserId(),
-			'parsed' => $group->getUserId()
-		];
-	}
 }
