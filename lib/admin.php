4294912d5e2db58996acc18b6bcf542cdaab4dae
Maxence Lange <maxence@artificial-owl.com>
2017-08-27 14:34:47 +0200

    automatic DI
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/admin.php
+++ lib/admin.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace OCA\Circles;
+use OCA\Circles\Controller\SettingsController;
 use OCP\AppFramework\Http\TemplateResponse;
 
 $app = new AppInfo\Application();
 
 /** @var TemplateResponse $response */
 $response = $app->getContainer()
-				->query('SettingsController')
+				->query(SettingsController::class)
 				->admin();
 
 return $response->render();
