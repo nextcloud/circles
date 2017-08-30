64d32b8211244d46fdecda60731e3b2454bd6ae9
Maxence Lange <maxence@artificial-owl.com>
2017-08-30 18:27:18 +0200

    make them translateable
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- templates/settings.admin.php
+++ templates/settings.admin.php
@@ -13,15 +13,16 @@ style(Application::APP_NAME, 'admin');
 
 	<table cellpadding="10" cellpadding="5">
 		<tr class="lane">
-			<td colspan="2" class="left">Allow Group Linking:<br/>
-				<em>Groups can be linked to Circles.</em></td>
+			<td colspan="2" class="left"><?php p($l->t('Allow Group Linking:')); ?><br/>
+				<em><?php p($l->t('Groups can be linked to Circles.')); ?></em></td>
 			<td class="right">
 				<input type="checkbox" value="1" id="allow_linked_groups"/>
 			</td>
 		</tr>
 		<tr class="lane">
-			<td colspan="2" class="left">Allow Federated Circles:<br/>
-				<em>Circles from different Nextclouds can be linked together.</em></td>
+			<td colspan="2" class="left"><?php p($l->t('Allow Federated Circles:')); ?><br/>
+				<em><?php p($l->t('Circles from different Nextclouds can be linked together.')); ?></em>
+			</td>
 			<td class="right">
 				<input type="checkbox" value="1" id="allow_federated_circles"/>
 			</td>
