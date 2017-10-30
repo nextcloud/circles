2cd831e7e759931be1b33230313ee4cfbdd0021a
Maxence Lange <maxence@artificial-owl.com>
2017-10-30 11:45:53 -0100

    Fixes #144
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Db/CircleProviderRequestBuilder.php
+++ lib/Db/CircleProviderRequestBuilder.php
@@ -527,7 +527,7 @@ class CircleProviderRequestBuilder {
 		$qb->addSelect('s.file_source', 's.file_target');
 		/** @noinspection PhpMethodParametersCountMismatchInspection */
 		$qb->from('share', 's')
-		   ->where($expr->eq('s.share_type', $qb->createNamedParameter(Share::SHARE_TYPE_CIRCLE)))
+		   ->andWhere($expr->eq('s.share_type', $qb->createNamedParameter(Share::SHARE_TYPE_CIRCLE)))
 		   ->andWhere(
 			   $expr->orX(
 				   $expr->eq('s.item_type', $qb->createNamedParameter('file')),
