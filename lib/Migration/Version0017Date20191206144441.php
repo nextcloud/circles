088b1ab69145edb840d926db2b4c1c17cc313568
Maxence Lange <maxence@artificial-owl.com>
2019-12-06 15:55:03 -0100

    migration
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

---
+++ lib/Migration/Version0017Date20191206144441.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Circles - Bring cloud-users closer together.
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Maxence Lange <maxence@artificial-owl.com>
+ * @copyright 2019
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace OCA\Circles\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * Auto-generated migration step: Please modify to your needs!
+ */
+class Version0017Date20191206144441 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function preSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		if ($schema->hasTable('circles_circles')) {
+			$table = $schema->createTable('circles_circles');
+			$table->addColumn('contact_addressbook', 'integer', [
+				'notnull' => false,
+				'unsigned' => true,
+				'length' => 7,
+			]);
+			$table->addColumn('contact_groupname', 'string', [
+				'notnull' => false,
+				'length' => 127,
+			]);
+		}
+
+		if ($schema->hasTable('circles_members')) {
+			$table = $schema->createTable('circles_members');
+			$table->addColumn('contact_id', 'string', [
+				'notnull' => false,
+				'length' => 63,
+			]);
+			$table->dropPrimaryKey();
+			$table->setPrimaryKey(['circle_id', 'user_id', 'contact_id']);
+		}
+
+		return $schema;
+	}
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 */
+	public function postSchemaChange(IOutput $output, Closure $schemaClosure, array $options) {
+	}
+}
