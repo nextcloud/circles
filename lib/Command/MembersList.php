66b006caffb1d4c58e2460a4bc4be2ed3fb15929
Maxence Lange <maxence@artificial-owl.com>
2021-07-26 23:47:37 -0100

    phpdoc
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Command/MembersList.php
+++ lib/Command/MembersList.php
@@ -40,6 +40,7 @@ use ArtificialOwl\MySmallPhpTools\Model\Nextcloud\nc22\NC22TreeNode;
 use ArtificialOwl\MySmallPhpTools\Model\SimpleDataStore;
 use ArtificialOwl\MySmallPhpTools\Traits\Nextcloud\nc22\TNC22ConsoleTree;
 use ArtificialOwl\MySmallPhpTools\Traits\TStringTools;
+use Exception;
 use OC\Core\Command\Base;
 use OCA\Circles\Db\MemberRequest;
 use OCA\Circles\Exceptions\CircleNotFoundException;
@@ -361,9 +362,9 @@ class MembersList extends Base {
 					$node = new NC22TreeNode(
 						$tree, new SimpleDataStore(
 								 [
-								 	'circle' => $circle,
-								 	'member' => $member,
-								 	'cycling' => in_array($member->getSingleId(), $knownIds),
+									 'circle' => $circle,
+									 'member' => $member,
+									 'cycling' => in_array($member->getSingleId(), $knownIds),
 								 ]
 							 )
 					);
@@ -381,8 +382,8 @@ class MembersList extends Base {
 						new NC22TreeNode(
 							$tree, new SimpleDataStore(
 									 [
-									 	'member' => $member,
-									 	'cycling' => in_array($member->getSingleId(), $knownIds)
+										 'member' => $member,
+										 'cycling' => in_array($member->getSingleId(), $knownIds)
 									 ]
 								 )
 						);
@@ -412,7 +413,10 @@ class MembersList extends Base {
 			$circle = null;
 			if ($data->hasKey('circle')) {
 				/** @var Circle $circle */
-				$circle = $data->gObj('circle', Circle::class);
+				try {
+					$circle = $data->gObj('circle', Circle::class);
+				} catch (Exception $e) {
+				}
 			}
 
 			if ($data->hasKey('member')) {
