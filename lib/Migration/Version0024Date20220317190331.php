583af2a26eb49ad427c7a633490855a3ac7cc95d
Maxence Lange <maxence@artificial-owl.com>
2022-03-18 11:32:03 -0100

    primaryKey on circles_membership
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Migration/Version0024Date20220317190331.php
+++ lib/Migration/Version0024Date20220317190331.php
@@ -31,20 +31,40 @@ declare(strict_types=1);
 
 namespace OCA\Circles\Migration;
 
-use OCP\IDBConnection;
+use Closure;
+use Doctrine\DBAL\Schema\SchemaException;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
 use OCP\Migration\SimpleMigrationStep;
 
-/**
- * Class Version0023Date20211216113101
- *
- * @package OCA\Circles\Migration
- */
-class Version0023Date20211216113101 extends SimpleMigrationStep {
-
+class Version0024Date20220317190331 extends SimpleMigrationStep {
 
 	/**
-	 * @param IDBConnection $connection
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 *
+	 * @return null|ISchemaWrapper
+	 * @throws SchemaException
 	 */
-	public function __construct(IDBConnection $connection) {
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		if ($schema->hasTable('circles_membership')) {
+			$table = $schema->getTable('circles_membership');
+			if (!$table->hasPrimaryKey()) {
+				$indexes = $table->getIndexes();
+				// conflict in Oracle with existing unique index, duplicate of primaryKey.
+				foreach ($indexes as $index) {
+					if ($index->isUnique()) {
+						$table->dropIndex($index->getName());
+					}
+				}
+				$table->setPrimaryKey(['single_id', 'circle_id']);
+			}
+		}
+
+		return $schema;
 	}
 }
