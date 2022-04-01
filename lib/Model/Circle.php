3b2b73e0e59c821beacfe9b0bf3587ed0068a264
Git'Fellow <carlos@reendex.com>
2022-04-01 18:46:26 +0200

    Oups

--- lib/Model/Circle.php
+++ lib/Model/Circle.php
@@ -914,6 +914,7 @@ class Circle extends ManagedModel implements IEntity, IDeserializable, IQueryRow
 		$creation = $this->get($prefix . 'creation', $data);
 		$dateTime = DateTime::createFromFormat('Y-m-d H:i:s', $creation);
 		$timestamp = $dateTime ? $dateTime->getTimestamp() : (int) strtotime('Y-m-d H:i:s', $creation);
+		$this->setCreation($timestamp);
 
 		$this->setPopulation($this->getInt('population', $this->getSettings()));
 		$this->setPopulationInherited($this->getInt('populationInherited', $this->getSettings()));
