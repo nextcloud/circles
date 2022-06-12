b56768fec89453ab39c01812fa805d7a4d25f2e5
Maxence Lange <maxence@artificial-owl.com>
2022-06-12 11:02:53 -0100

    confirmKey must be uuid
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Tools/Traits/TStringTools.php
+++ lib/Tools/Traits/TStringTools.php
@@ -86,6 +86,19 @@ trait TStringTools {
 
 
 	/**
+	 * @param string $uuid
+	 *
+	 * @return bool
+	 */
+	protected function isUuid(string $uuid): bool {
+		if ($uuid === '') {
+			return false;
+		}
+
+		return (preg_match('/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i', $uuid) === 1);
+	}
+
+	/**
 	 * @param string $line
 	 * @param int $length
 	 *
