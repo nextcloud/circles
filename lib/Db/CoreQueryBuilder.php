a93b7309b1fbe1f1d7a0a292310d5c01720210a5
Maxence Lange <maxence@artificial-owl.com>
2023-07-05 23:44:16 -0100

    ignore personal's ownership requirement
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Db/CoreQueryBuilder.php
+++ lib/Db/CoreQueryBuilder.php
@@ -1388,7 +1388,7 @@ class CoreQueryBuilder extends ExtendedQueryBuilder {
 				$orXLevelCheck->add(
 					$this->expr()->gte(
 						$alias . '.level',
-						$this->createNamedParameter($minimumLevel)
+						$this->createNamedParameter($minimumLevel, self::PARAM_INT)
 					)
 				);
 			},
