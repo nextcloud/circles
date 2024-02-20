a2d519c5da0d0a4c25d1275d53c2694df90a65e0
Maxence Lange <maxence@artificial-owl.com>
2024-02-20 12:18:38 -0100

    circles' activiy
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Activity/SettingAsMember.php
+++ lib/Activity/SettingAsMember.php
@@ -6,15 +6,9 @@ use OCP\Activity\ISetting;
 use OCP\IL10N;
 
 class SettingAsMember implements ISetting {
-	/** @var IL10N */
-	protected $l10n;
-
-
-	/**
-	 * @param IL10N $l10n
-	 */
-	public function __construct(IL10N $l10n) {
-		$this->l10n = $l10n;
+	public function __construct(
+		protected IL10N $l10n
+	) {
 	}
 
 
