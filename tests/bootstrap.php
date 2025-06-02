465fdee552512f733ebb4ec660f9118d22b6b320
provokateurin <kate@provokateurin.de>
2025-06-02 10:43:40 +0200

    test: Fix autoloading
    Signed-off-by: provokateurin <kate@provokateurin.de>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * SPDX-FileCopyrightText: 2017 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
@@ -9,7 +11,7 @@ define('PHPUNIT_RUN', 1);
 require_once __DIR__ . '/../../../lib/base.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
-\OC::$composerAutoloader->addPsr4('Tests\\', OC::$SERVERROOT . '/tests/unit/', true);
+require_once __DIR__ . '/../../../tests/autoload.php';
 
 \OC_App::loadApp('circles');
 
