f1cf55c7f5b01734e9aebe0541fd356006121761
Ferdinand Thiessen <opensource@fthiessen.de>
2025-08-19 21:24:14 +0200

    fix(dashboard): properly create a frontend logger instance
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/logger.ts
+++ src/logger.ts
@@ -3,19 +3,10 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { getCurrentUser } from '@nextcloud/auth'
 import { getLoggerBuilder } from '@nextcloud/logger'
 
-const getLogger = user => {
-	if (user === null) {
-		return getLoggerBuilder()
-			.setApp('circles')
-			.build()
-	}
-	return getLoggerBuilder()
-		.setApp('circles')
-		.setUid(user.uid)
-		.build()
-}
-
-export default getLogger(getCurrentUser())
+export const logger = getLoggerBuilder()
+	.setApp('teams')
+	.detectUser()
+	.detectLogLevel()
+	.build()
