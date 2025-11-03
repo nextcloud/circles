7951cc9129995b0122a1603fc3e8238c94cef2cc
Maxence Lange <maxence@artificial-owl.com>
2025-11-03 21:05:46 -0100

    refactoring activities
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

--- lib/Activity/Deprecated/ProviderSubjectCircle.php
+++ lib/Activity/Deprecated/ProviderSubjectCircle.php
@@ -7,19 +7,26 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-namespace OCA\Circles\Activity;
+namespace OCA\Circles\Activity\Deprecated;
 
 use OCA\Circles\Exceptions\FakeException;
+use OCA\Circles\Model\Circle;
 use OCP\Activity\IEvent;
 
 class ProviderSubjectCircle extends ProviderParser {
-	public function parseSubjectCircleCreate(IEvent $event, array $params): void {
+	/**
+	 * @param IEvent $event
+	 * @param Circle $circle
+	 *
+	 * @throws FakeException
+	 */
+	public function parseSubjectCircleCreate(IEvent $event, Circle $circle): void {
 		if ($event->getSubject() !== 'circle_create') {
 			return;
 		}
 
 		$this->parseCircleEvent(
-			$event, $params,
+			$event, $circle,
 			$this->l10n->t('You created the team {circle}'),
 			$this->l10n->t('{author} created the team {circle}')
 		);
@@ -29,17 +36,17 @@ class ProviderSubjectCircle extends ProviderParser {
 
 	/**
 	 * @param IEvent $event
-	 * @param array $params
+	 * @param Circle $circle
 	 *
 	 * @throws FakeException
 	 */
-	public function parseSubjectCircleDelete(IEvent $event, array $params): void {
+	public function parseSubjectCircleDelete(IEvent $event, Circle $circle): void {
 		if ($event->getSubject() !== 'circle_delete') {
 			return;
 		}
 
 		$this->parseCircleEvent(
-			$event, $params,
+			$event, $circle,
 			$this->l10n->t('You deleted {circle}'),
 			$this->l10n->t('{author} deleted {circle}')
 		);
