ab2ab916fa8817b6c9207ae40673e1bfc30b1ec2
Kostiantyn Miakshyn <molodchick@gmail.com>
2025-11-29 23:38:19 +0100

    feat: Add the possibility to create invitation links to circles
    Signed-off-by: Kostiantyn Miakshyn <molodchick@gmail.com>

---
+++ lib/FederatedItems/CircleRevokeInvitation.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2021 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Circles\FederatedItems;
+
+use OCA\Circles\Db\CircleInvitationRequest;
+use OCA\Circles\IFederatedItem;
+use OCA\Circles\Model\Federated\FederatedEvent;
+use OCA\Circles\Model\Helpers\MemberHelper;
+use OCA\Circles\Service\EventService;
+use OCA\Circles\Tools\Traits\TDeserialize;
+
+class CircleRevokeInvitation implements IFederatedItem {
+	use TDeserialize;
+
+	public function __construct(
+		private CircleInvitationRequest $circleInvitationRequest,
+		private EventService $eventService,
+	) {
+	}
+
+	public function verify(FederatedEvent $event): void {
+		$circle = $event->getCircle();
+
+		$initiatorHelper = new MemberHelper($circle->getInitiator());
+		$initiatorHelper->mustBeAdmin();
+
+		$new = clone $circle;
+		$new->setCircleInvitation(null);
+
+		$event->setOutcome($this->serialize($new));
+	}
+
+	public function manage(FederatedEvent $event): void {
+		$circle = clone $event->getCircle();
+
+		$this->circleInvitationRequest->delete($circle->getSingleId());
+		// todo: do we need separate event here?
+		$this->eventService->circleEditing($event);
+	}
+
+	public function result(FederatedEvent $event, array $results): void {
+		// todo: do we need separate event here?
+		$this->eventService->circleEdited($event, $results);
+	}
+}
