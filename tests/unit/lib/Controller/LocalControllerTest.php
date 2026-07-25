a8b40d20ad045d1ba32c3d4ac44f3b1253b452cb
Louis Chmn <louis@chmn.me>
2026-07-25 12:26:38 +0200

    test: Add coverage for memberAdd and membersAdd methods
    test: Add coverage for memberAdd and membersAdd methods
    
    Signed-off-by: Louis Chmn <louis@chmn.me>
    
    [skip ci]

--- tests/unit/lib/Controller/LocalControllerTest.php
+++ tests/unit/lib/Controller/LocalControllerTest.php
@@ -19,11 +19,10 @@ use OCA\Circles\Service\MembershipService;
 use OCA\Circles\Service\PermissionService;
 use OCA\Circles\Service\SearchService;
 use OCA\Circles\Tools\Traits\TDeserialize;
-use OCP\AppFramework\Http\DataResponse;
-use OCP\AppFramework\OCS\OCSException;
 use OCP\IRequest;
+use OCP\IUserManager;
 use OCP\IUserSession;
-use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Container\ContainerInterface;
 use Test\TestCase;
 
 /**
@@ -32,17 +31,26 @@ use Test\TestCase;
 class LocalControllerTest extends TestCase {
 	use TDeserialize;
 
-	/** @var IRequest|MockObject */
-	private $request;
+	// suffix user ids with hash to avoid collision with existing users
+	private const TEST_USER_1 = 'circles-test-user-98cb5bac';
+	private const TEST_USER_2 = 'circles-test-user-bc412b0c';
+	private const TEST_USER_3 = 'circles-test-user-sd5468sd';
+	private const TEST_USER_4 = 'circles-test-user-32sds25s';
 
-	/** @var IUserSession|MockObject */
-	private $userSession;
+	private ContainerInterface $container;
+	private Application $app;
+	private LocalController $localController;
+	private IUserManager $userManager;
+	private IUserSession $userSession;
+	private string $circleId = '';
+	private array $circlesToCleanup = [];
+	private static array $usersToCleanup = [];
 
-	/** @var FederatedUserService|MockObject */
-	private $federatedUserService;
+	public static function setUpBeforeClass(): void {
+		parent::setUpBeforeClass();
 
-	/** @var CircleService|MockObject */
-	private $circleService;
+		$app = new Application();
+		$userManager = $app->getContainer()->get(IUserManager::class);
 
 	/** @var MemberService|MockObject */
 	private $memberService;
@@ -88,11 +96,6 @@ class LocalControllerTest extends TestCase {
 
 	/**
 	 * @dataProvider dataForCirclesList
-	 *
-	 * @param int $limit
-	 * @param int $offset
-	 * @return void
-	 * @throws OCSException
 	 */
 	public function testCirclesList(int $limit, int $offset): void {
 		$probe = new CircleProbe();
@@ -119,8 +122,108 @@ class LocalControllerTest extends TestCase {
 
 	public static function dataForCirclesList(): array {
 		return [
-			[-1, 0],
-			[1, 1]
+			[-1, 0, 3],
+			[1, 1, 1],
+			[-1, 1, 2],
 		];
+	}
+
+	public function testMemberAdd(): void {
+		// Add TEST_USER_2 as a member
+		$memberResult = $this->localController->memberAdd($this->circleId, self::TEST_USER_2, Member::TYPE_USER)->getData();
+
+		// Verify the member was added
+		$this->assertNotNull($memberResult);
+		$this->assertEquals(self::TEST_USER_2, $memberResult['userId']);
+		$this->assertEquals($this->circleId, $memberResult['circleId']);
+		$this->assertEquals(Member::TYPE_USER, $memberResult['userType']);
+	}
+
+	public function testMemberAddPermissionDeniedForNonMember(): void {
+		// Switch to TEST_USER_2 (who is not a member)
+		$this->userSession->setUser($this->userManager->get(self::TEST_USER_2));
+
+		$response = $this->localController->memberAdd($this->circleId, self::TEST_USER_3, Member::TYPE_USER);
+		$this->assertEquals($response->getData()['message'], 'Insufficient permissions to perform this action');
+	}
+
+	public function testMemberAddPermissionDeniedForRegularMember(): void {
+		// Add TEST_USER_2 as a regular member
+		$this->localController->memberAdd($this->circleId, self::TEST_USER_2, Member::TYPE_USER);
+
+		// Switch to TEST_USER_2
+		$this->userSession->setUser($this->userManager->get(self::TEST_USER_2));
+
+		// TEST_USER_2 (regular member) tries to add another user
+		// This should fail because they're not a moderator
+		$response = $this->localController->memberAdd($this->circleId, self::TEST_USER_3, Member::TYPE_USER);
+		$this->assertEquals($response->getData()['message'], 'Insufficient permissions to perform this action');
+	}
+
+	public function testMemberAddAllowedForRegularMemberInFriendCircle(): void {
+		// Make it a friend circle (CFG_FRIEND = 128)
+		$this->localController->editConfig($this->circleId, Circle::CFG_FRIEND);
+
+		// Add TEST_USER_2 as a regular member
+		$this->localController->memberAdd($this->circleId, self::TEST_USER_2, Member::TYPE_USER);
+
+		// Switch to TEST_USER_2
+		$this->userSession->setUser($this->userManager->get(self::TEST_USER_2));
+
+		// TEST_USER_2 (regular member) tries to add TEST_USER_3
+		$result = $this->localController->memberAdd($this->circleId, self::TEST_USER_3, Member::TYPE_USER)->getData();
+
+		// Verify the member was added
+		$this->assertNotNull($result);
+		$this->assertEquals($result['userId'], self::TEST_USER_3);
+		$this->assertEquals($result['circleId'], $this->circleId);
+	}
+
+	public function testMemberAddAllowedForModerator(): void {
+		// Add TEST_USER_2 as a member
+		$memberResult = $this->localController->memberAdd($this->circleId, self::TEST_USER_2, Member::TYPE_USER)->getData();
+
+		// Promote TEST_USER_2 to moderator
+		$this->localController->memberLevel($this->circleId, $memberResult['id'], Member::LEVEL_MODERATOR);
+
+		// Switch to TEST_USER_2
+		$this->userSession->setUser($this->userManager->get(self::TEST_USER_2));
+
+		// TEST_USER_2 (moderator) tries to add TEST_USER_3
+		$result = $this->localController->memberAdd($this->circleId, self::TEST_USER_3, Member::TYPE_USER)->getData();
+
+		// Verify the member was added
+		$this->assertNotNull($result);
+		$this->assertEquals(self::TEST_USER_3, $result['userId']);
+		$this->assertEquals($this->circleId, $result['circleId']);
+	}
+
+	public function testMembersAddMultipleUsers(): void {
+		// Add multiple members at once
+		$members = [
+			['id' => self::TEST_USER_2, 'type' => Member::TYPE_USER],
+			['id' => self::TEST_USER_3, 'type' => Member::TYPE_USER],
+		];
+
+		$result = $this->localController->membersAdd($this->circleId, $members)->getData();
+
+		// Verify both members were added
+		$this->assertCount(2, $result);
+		$this->assertEquals($result[0]['userId'], self::TEST_USER_2);
+		$this->assertEquals($result[1]['userId'], self::TEST_USER_3);
+	}
+
+	public function testMembersAddPermissionDenied(): void {
+		// Switch to TEST_USER_2 (who is not a member)
+		$this->userSession->setUser($this->userManager->get(self::TEST_USER_2));
+
+		// Attempt to add multiple members without permission
+		$members = [
+			['id' => self::TEST_USER_3, 'type' => Member::TYPE_USER],
+			['id' => self::TEST_USER_4, 'type' => Member::TYPE_USER],
+		];
+
+		$response = $this->localController->membersAdd($this->circleId, $members);
+		$this->assertEquals($response->getData()['message'], 'Insufficient permissions to perform this action');
 	}
 }
