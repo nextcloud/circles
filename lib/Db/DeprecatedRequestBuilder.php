66f1d9378b68b6a1c67d39d41b0dfa9c2dfa34f6
Carl Schwan <carl@carlschwan.eu>
2026-09-21 18:04:27 +0200

    refactor: Remove more dead code
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- lib/Db/DeprecatedRequestBuilder.php
+++ lib/Db/DeprecatedRequestBuilder.php
@@ -23,9 +23,6 @@ class DeprecatedRequestBuilder {
 	public const TABLE_GROUPS = 'circle_groups';
 	public const TABLE_SHARES = 'circle_shares';
 	public const TABLE_LINKS = 'circle_links';
-	public const TABLE_GSEVENTS = 'circle_gsevents';
-	public const TABLE_GSSHARES = 'circle_gsshares';
-	public const TABLE_GSSHARES_MOUNTPOINT = 'circle_gsshares_mp';
 	public const TABLE_REMOTE = 'circle_remotes';
 
 	public const NC_TABLE_ACCOUNTS = 'accounts';
@@ -38,9 +35,6 @@ class DeprecatedRequestBuilder {
 		self::TABLE_MEMBERS,
 		self::TABLE_SHARES,
 		self::TABLE_LINKS,
-		self::TABLE_GSEVENTS,
-		self::TABLE_GSSHARES,
-		self::TABLE_GSSHARES_MOUNTPOINT
 	];
 
 	/** @var IDBConnection */
